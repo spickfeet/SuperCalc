2b633c5d79e4fed7cc80ac8760f95cb84509f494
spickfeet <slavaspickfeet@mail.ru>
2024-05-17 09:54:08 +0300

    Изменены исключения.

--- SuperCalc/Methods/MethodsFactory.cs
+++ SuperCalc/Methods/MethodsFactory.cs
@@ -62,7 +62,7 @@ namespace SuperCalc.Methods
                 for (int i = 0; i < expressions.Length; i++)
                 {
                     if (expressions[i][0] == '"' || expressions[i][expressions[i].Length - 1] == '"') 
-                        throw new Exception("Аргумент не является числом");
+                        throw new ArgumentException("Аргумент не является числом");
 
                     values[i] = (decimal)RPN.Calculate(expressions[i].ToString());
                 }
@@ -81,7 +81,7 @@ namespace SuperCalc.Methods
                     if (i == 2 && expressions.Length == 3)
                     {
                         if (expressions[i][0] == '"' || expressions[i][expressions[i].Length - 1] == '"')
-                            throw new Exception("Аргумент не является числом");
+                            throw new ArgumentException("Аргумент не является числом");
 
                         values[i] = RPN.Calculate(expressions[i]).ToString();
 
@@ -132,7 +132,7 @@ namespace SuperCalc.Methods
                 for (int i = 0; i < expressions.Length; i++)
                 {
                     if (expressions[i][0] == '"' || expressions[i][expressions[i].Length - 1] == '"') 
-                        throw new Exception("Аргумент не является числом");
+                        throw new ArgumentException("Аргумент не является числом");
 
                     values[i] = (decimal)RPN.Calculate(expressions[i].ToString());
                 }
